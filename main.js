e2cd6dd410d6c446fce3f967f524306712a0516e
katyng1130 <nhumai.ktqd@gmail.com>
2023-03-15 23:24:25 +0800

    test

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 console.log("hi   this is branch 02");
-<<<<<<< HEAD
-console.log("hi   this is branch main");
-=======
 console.log("hi   this is branch 03");
->>>>>>> acd2bffc6950bf4e8dc1c2287104f5703dc3dfb0
